c8f069a9053351b390894de9c934c30dc69cd7c0
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-09-02 10:01:56 +0200

    wip: terraform plan task added

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -1,19 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.42.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-    
-  }
+  features {}
 }
 
 resource "azurerm_resource_group" "learn" {
   name     = "rg-learn-iac"
-  location = "eastus"
+  location = "East US"
 }
